298d454c8de28cc21075a4fa70dc3c9a88e2c3e2
Nate Deakers <ndeakers@gmail.com>
2021-05-13 12:28:47 -0700

    created two get routes

--- routes/companies.js
+++ routes/companies.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const router = new express.Router();
-const db = require("../db.js");
+const db = require("../db");
 
 
 
@@ -9,11 +9,27 @@ module.exports = router;
 
 //GET / companies
 // returns  list of companies, like {companies: [{code, name}, ...]}
-
+router.get("/", async function (req, res, next) {
+  const results = await db.query(
+    `SELECT code, name, description
+    FROM companies`);
+  console.log("result object: ", results)
+  const companies = results.rows;
+  return res.json({ companies });
+})
 
 
 //GET/companies/[code] 
 // Return obj of company: {company: {code, name, description}}
+router.get("/:code", async function (req, res, next) {
+  const code = req.params.code;
+  const results = await db.query(
+    `SELECT code, name, description
+             FROM companies
+             WHERE code = $1`, [code]);
+  let company = results.rows[0];
+  return res.json({ company });
+})
 
 
 
--- routes/invoices.js
+++ routes/invoices.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const router = new express.Router();
-const db = require("../db.js");
+const db = require("../db");
 
 
 
